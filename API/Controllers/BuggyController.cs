acdd507c22273f37c0ad2e1dc24bc047d08f21c5
Mateusz Kopówka <mkopowka@hotmail.com>
2023-04-02 00:03:19 +0200

    Error Handling

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,40 @@
+using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+
+        }
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
