a27f8ddbd27c62520914ef53ad86180e57663ac6
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-21 16:20:18 +0200

    test

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.cd ..
+
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
