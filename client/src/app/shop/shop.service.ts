e8d1b0a48f5838d1cf92b5adba2b49b225ccf945
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-22 17:24:18 +0200

    Paging Searching Filtering

---
+++ client/src/app/shop/shop.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IPagination } from '../shared/models/pagination';
+import { IProduct } from '../shared/models/product';
+import { IBrand } from '../shared/models/type';
+import { IType } from '../shared/models/brand';
+import { ShopParams } from '../shared/models/shopParams';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl ='https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(shopParams: ShopParams) {
+    let params = new HttpParams();
+
+    if(shopParams.brandId) params = params.append('brandId',shopParams.brandId);
+    if(shopParams.typeId) params = params.append('typeId',shopParams.typeId);
+    params = params.append('sort',shopParams.sort);
+    params = params.append('pageIndex',shopParams.pageNumber);
+    params = params.append('pageSize',shopParams.pageSize);
+    if(shopParams.search) params = params.append('search',shopParams.search);
+
+    return this.http.get<IPagination<IProduct[]>>(this.baseUrl+'products',{params});
+  }
+
+  getBrands() {
+    return this.http.get<IBrand[]>(this.baseUrl+ 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<IType[]>(this.baseUrl+ 'products/types');
+  }
+}
