0b999916255026c450e141473edde1b5816e8950
Mateusz Kopówka <mkopowka@hotmail.com>
2023-08-22 10:59:05 +0200

    TypeScript products

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {IProduct} from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Ski Snow Store';
-  products: any[] = [];
+  products: IProduct[] = []
   constructor(private http:HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
+    this.http.get<IPagination<IProduct[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
       next: (response: any) => this.products = response.data,
       error: error => console.log(error),
       complete: () => {
